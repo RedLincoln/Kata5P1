8675c2816951260322083f896920081465a17fc8
RedLincoln <miguel_lcp@hotmail.com>
2018-12-04 08:36:43 +0000

    Kata5P1 version 3

--- Kata5P1/src/kata5p1/Kata5P1.java
+++ Kata5P1/src/kata5p1/Kata5P1.java
@@ -1,13 +1,50 @@
 package kata5p1;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
+import kata5p1.view.MailListReader;
 
 public class Kata5P1 {
-
+    
+     public static void main(String[] args) throws SQLException, IOException {
+        Kata5P1 kata5p1 = new Kata5P1();
+        kata5p1.selectAll("PEOPLE");
+        
+        //
+        
+        createNewTable();
+    
+        
+        // 
+        
+        String emailPath = "email.txt";
+        List<String> emailList = MailListReader.read(emailPath);
+        for (String mail : emailList) {
+           kata5p1.insert(mail, "Kata5.db");
+        }
+        
+     }
+     
+     
+     public void insert(String mail, String database) throws SQLException{
+        String sql = "INSERT INTO EMAIL(mail) VALUES (?)";
+        try (Connection conn = this.connect(database);
+             PreparedStatement pstmt = conn.prepareStatement(sql)){
+            
+            pstmt.setString(1, mail);
+            pstmt.executeUpdate();
+        }catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }   
+     }
+    
+     
     public Connection connect(String datebase){
         String url = "jdbc:sqlite:" + datebase;
         Connection conn = null;
@@ -38,15 +75,7 @@ public class Kata5P1 {
     }
     
     
-    public static void main(String[] args) throws SQLException {
-        Kata5P1 kata5p1 = new Kata5P1();
-        kata5p1.selectAll("PEOPLE");
-        
-        //
-        
-        createNewTable();
-    }
-    
+   
     public static void createNewTable() {
         String url = "jdbc:sqlite:Kata5.db";
         String sql = "CREATE TABLE IF NOT EXISTS EMAIL (\n"
