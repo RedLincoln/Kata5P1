d6146662e06a5003c559815e12e59d20019bbd6d
RedLincoln <miguel_lcp@hotmail.com>
2018-12-04 08:21:15 +0000

    Kata5P1 version2

--- Kata5P1/src/kata5p1/Kata5P1.java
+++ Kata5P1/src/kata5p1/Kata5P1.java
@@ -8,10 +8,22 @@ import java.sql.Statement;
 
 public class Kata5P1 {
 
-    public static void main(String[] args) throws SQLException {
-        String url = "jdbc:sqlite:Kata5.db";
+    public Connection connect(String datebase){
+        String url = "jdbc:sqlite:" + datebase;
+        Connection conn = null;
+        try { 
+             conn = DriverManager.getConnection(url);
+        }catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+    
+    
+    
+    public void selectAll(String table){
         String sql = "SELECT * FROM PEOPLE";
-        try ( Connection conn = DriverManager.getConnection(url);
+        try ( Connection conn = this.connect("Kata5.db");
               Statement stmt = conn.createStatement();
               ResultSet rs = stmt.executeQuery(sql)){
             while(rs.next()) {
@@ -23,6 +35,31 @@ public class Kata5P1 {
         }catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+    }
+    
+    
+    public static void main(String[] args) throws SQLException {
+        Kata5P1 kata5p1 = new Kata5P1();
+        kata5p1.selectAll("PEOPLE");
+        
+        //
+        
+        createNewTable();
+    }
+    
+    public static void createNewTable() {
+        String url = "jdbc:sqlite:Kata5.db";
+        String sql = "CREATE TABLE IF NOT EXISTS EMAIL (\n"
+                + " id integer PRIMARY KEY AUTOINCREMENT, \n"
+                + " mail text NOT NULL);";
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement ()) {
+            
+            stmt.execute(sql);
+            System.out.println("Tabla creada");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+         }       
     }
     
 }
